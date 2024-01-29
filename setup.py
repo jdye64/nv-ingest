fb64c81faca756236a9a7109505efaeac025c7f5
Devin Robison <drobison@nvidia.com>
2024-01-29 15:10:58 -0700

    Cleanup

--- setup.py
+++ setup.py
@@ -8,18 +8,7 @@ setup(
     author_email='drobison@nvidia.com',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    install_requires=[
-        # List your project's dependencies here.
-        # e.g., 'numpy', 'pandas', 'PyPDF2', etc.
-    ],
-    classifiers=[
-        # Choose your license as you wish
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Operating System :: OS Independent',
-    ],
+    install_requires=[],
+    classifiers=[],
     python_requires='>=3.7',
 )
