94448452cce90769457a4169b10c0ba850343bc1
Edward Kim <edwardk@nvidia.com>
2024-04-29 19:46:13 -0700

    Extend filter_by_task to also filter by task properties

--- src/nv_ingest/util/flow_control/filter_by_task.py
+++ src/nv_ingest/util/flow_control/filter_by_task.py
@@ -13,10 +13,9 @@ from functools import wraps
 
 def filter_by_task(required_tasks, forward_func=None):
     """
-    A decorator that checks if the first argument to the wrapped function (expected to be a
-    ControlMessage object)
-    contains any of the tasks specified in `required_tasks`. If the message does not contain any
-    listed task,
+    A decorator that checks if the first argument to the wrapped function (expected to be a ControlMessage object)
+    contains any of the tasks specified in `required_tasks`. Each task can be a string of the task name or a tuple
+    of the task name and task properties. If the message does not contain any listed task and/or task properties,
     the message is returned directly without calling the wrapped function, unless a forwarding
     function is provided, in which case it calls that function on the ControlMessage.
 
@@ -37,13 +36,26 @@ def filter_by_task(required_tasks, forward_func=None):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            if args and hasattr(args[0], "has_task"):
+            if args and hasattr(args[0], "get_tasks"):
                 message = args[0]
-                if any(message.has_task(task) for task in required_tasks):
-                    return func(*args, **kwargs)
-                elif forward_func:
+                tasks = message.get_tasks()
+                for required_task in required_tasks:
+                    if isinstance(required_task, str) and (required_task in tasks):
+                        return func(*args, **kwargs)
+                    if isinstance(required_task, tuple) or isinstance(required_task, list):
+                        required_task, *required_task_props_list = required_task
+                        if required_task not in tasks:
+                            continue
+                        task_props = tasks.get(required_task, [None])
+                        if not task_props:
+                            continue
+                        task_props = task_props.pop()
+                        for required_task_props in required_task_props_list:
+                            if _is_subset(task_props, required_task_props):
+                                return func(*args, **kwargs)
+                if forward_func:
                     # If a forward function is provided, call it with the ControlMessage
-                    forward_func(message)
+                    return forward_func(message)
                 else:
                     # If no forward function is provided, return the message directly
                     return message
@@ -55,3 +67,15 @@ def filter_by_task(required_tasks, forward_func=None):
         return wrapper
 
     return decorator
+
+
+def _is_subset(superset, subset):
+    match subset:
+        case str():
+            return subset in superset
+        case dict():
+            return all(key in superset and _is_subset(val, superset[key]) for key, val in subset.items())
+        case list() | set():
+            return all(any(_is_subset(subitem, superitem) for superitem in superset) for subitem in subset)
+        case _:
+            return subset == superset
--- tests/nv_ingest/util/flow_control/test_filter_by_task.py
+++ tests/nv_ingest/util/flow_control/test_filter_by_task.py
@@ -9,6 +9,7 @@ from nv_ingest.util.flow_control.filter_by_task import filter_by_task
 def mock_control_message():
     # Create a mock ControlMessage object
     control_message = Mock()
+    control_message.payload.return_value = "not processed"
 
     # Default to False for unspecified tasks
     control_message.has_task.return_value = False
@@ -16,31 +17,44 @@ def mock_control_message():
     # To simulate has_task returning True for a specific task ("task1")
     control_message.has_task.side_effect = lambda task: task == "task1"
 
+    # To simulate get_tasks for a specific task "task1"
+    control_message.get_tasks.return_value = {"task1": [{"prop1": "foo"}]}
+
     return control_message
 
 
 # Sample function to be decorated
 def process_message(message):
-    return message
-
-
-# Forward function example
-def forward_message(message):
+    message.payload.return_value = "processed"
     return message
 
 
 def test_filter_by_task_with_required_task(mock_control_message):
     decorated_func = filter_by_task(["task1"])(process_message)
     assert (
-        decorated_func(mock_control_message) == mock_control_message
+        decorated_func(mock_control_message).payload() == "processed"
     ), "Should process the message when required task is present."
+
+
+def test_filter_by_task_with_required_task_properties(mock_control_message):
+    decorated_func = filter_by_task([("task1", {"prop1": "foo"})])(process_message)
+    assert (
+        decorated_func(mock_control_message).payload() == "processed"
+    ), "Should process the message when both required task and required property are present."
 
 
 def test_filter_by_task_without_required_task_no_forward_func(mock_control_message):
     decorated_func = filter_by_task(["task3"])(process_message)
     assert (
-        decorated_func(mock_control_message) == mock_control_message
+        decorated_func(mock_control_message).payload() == "not processed"
     ), "Should return the original message when required task is not present and no forward_func is provided."
+
+
+def test_filter_by_task_without_required_task_properteies_no_forward_func(mock_control_message):
+    decorated_func = filter_by_task([("task1", {"prop1": "bar"})])(process_message)
+    assert (
+        decorated_func(mock_control_message).payload() == "not processed"
+    ), "Should return the original message when required task is present but required task property is not present."
 
 
 def test_filter_by_task_without_required_task_with_forward_func(mock_control_message):
@@ -60,7 +74,27 @@ def test_filter_by_task_without_required_task_with_forward_func(mock_control_mes
     forward_func.assert_called_once_with(mock_control_message)
 
     # Assert that the result of calling the decorated function is as expected
-    assert result is None, "Should return the mock_control_message from the forward function."
+    assert result == mock_control_message, "Should return the mock_control_message from the forward function."
+
+
+def test_filter_by_task_without_required_task_properties_with_forward_func(mock_control_message):
+    # Create a simple mock function to be decorated
+    mock_function = Mock(return_value="some_value")
+
+    # Setup the forward function
+    forward_func = Mock(return_value=mock_control_message)
+
+    # Apply the decorator to the mock function
+    decorated_func = filter_by_task([("task1", {"prop1": "bar"})], forward_func=forward_func)(mock_function)
+
+    # Call the decorated function with the control message
+    result = decorated_func(mock_control_message)
+
+    # Check if forward_func was called since required task is not present
+    forward_func.assert_called_once_with(mock_control_message)
+
+    # Assert that the result of calling the decorated function is as expected
+    assert result == mock_control_message, "Should return the mock_control_message from the forward function."
 
 
 def test_filter_by_task_with_invalid_argument():
