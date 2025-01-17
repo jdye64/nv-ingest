27d6bcd9c93bd2b4f3ae210bfbe476cbeb27be7d
Jeremy Dyer <jdye64@gmail.com>
2025-01-17 12:25:02 -0500

    Introduce the common Python module for nv-ingest

---
+++ common/src/nv_ingest_common/pdf_service.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES.
+# All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+
+class PDFService:
+
+    def __init__(self):
+        self.pdf_parser = "pdfium"
