4fa3adb92623ac49169afa9e88dade8b017bc9a5
Jeremy Dyer <jdyer@nvidia.com>
2026-06-25 12:10:39 -0400

    perf: test page element batch 8

--- scripts/dgx_spark_perf_benchmark.py
+++ scripts/dgx_spark_perf_benchmark.py
@@ -273,7 +273,7 @@ presets:
     pdf_extract_num_cpus: 2.0
     pdf_extract_batch_size: 4
     pdf_split_batch_size: 1
-    page_elements_batch_size: 4
+    page_elements_batch_size: 8
     page_elements_workers: 3
     ocr_workers: 1
     ocr_batch_size: 8
