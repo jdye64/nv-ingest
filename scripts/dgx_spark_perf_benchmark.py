a97a1fcca42ec1005717ae0203d38c84cc8c0b42
Jeremy Dyer <jdyer@nvidia.com>
2026-06-25 12:35:56 -0400

    perf: test PDF split batch 4

--- scripts/dgx_spark_perf_benchmark.py
+++ scripts/dgx_spark_perf_benchmark.py
@@ -280,7 +280,7 @@ presets:
     pdf_extract_workers: 8
     pdf_extract_num_cpus: 2.0
     pdf_extract_batch_size: 4
-    pdf_split_batch_size: 2
+    pdf_split_batch_size: 4
     page_elements_batch_size: 4
     page_elements_workers: 3
     ocr_workers: 1
